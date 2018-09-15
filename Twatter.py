8404170166db26f7c15157bf30cd6b42c2333394
jfarley248 <32943754+jfarley248@users.noreply.github.com>
2018-09-14 23:29:33 -0400

    Update Twatter.py
    Alpha Release. Only parses Tweets and user account info

--- Twatter.py
+++ Twatter.py
@@ -17,6 +17,9 @@ import sqlite3
 from slimit import ast
 from slimit.parser import Parser
 from slimit.visitors import nodevisitor
+import twitter
+import json
+import re
 
 
 
@@ -47,7 +50,8 @@ def get_argument():
     parser = argparse.ArgumentParser(description='Utility to parse out Twitter Archive')
 
     #  Gets path to Twitter Archive
-    parser.add_argument('-a', '--archive', type=str, nargs='+', dest='archivePath', help='Path to Twitter Archive (.zip)')
+    parser.add_argument('-a', '--archive', type=str, nargs='+', dest='archivePath', help='Path to Twitter Archive')
+    #parser.add_argument('-o', '--output', type=str, nargs='+', dest='outputPath', help='Filepath to output database')
 
     args = parser.parse_args()
     if args.archivePath[0]:
@@ -61,63 +65,131 @@ def get_argument():
         else:
             print("Not a valid directory \n")
             sys.exit()
-        
-# Function to parse account information from account.js
-def account(path):
+    
 
-    data = []
-
-    # Query to create table for data
-    query = """
-
-        CREATE TABLE IF NOT EXISTS Account_Info (
-            Username text,
-            Display_Name text,
-            Email text,
-			Phone_Num text,
-            Account_ID int,
-			Created_At,
-			Created_Via text,
-			Timezone text
-                                    );
-
-    """
-    # Execute Query
-    db = open_db(DATABASE)
-    db.execute(query)
-
-    path = path + "\\account.js"
-    file = open(path, "r")
+# Function that returns array of values from javascript files
+def getDict(filepath):
+    dataArr = []
+    file = open(filepath, "r")
     parser = Parser()
     tree = parser.parse(file.read())
     fields = {getattr(node.left, 'value', ''): getattr(node.right, 'value', '')
           for node in nodevisitor.visit(tree)
           if isinstance(node, ast.Assign)}
-    for keys, values in fields.iteritems():
-        newKeys = keys.strip("\"")
-        newValues = values.strip("\"")
-        
-        fields[newKeys] =fields.pop(keys)
-        fields[newKeys] = newValues
 
+    # Edit the values obtained from the .js and append them to their own array
     for keys, values in fields.iteritems():
-        if values:
-            data.append(values)
-        else:
-            continue
+        newValues = values.strip("\"")
+        dataArr.append(str(newValues))
     
-    for items in data:
-        db.execute('insert into Account_Info items (?,?,?)', items)   
-    
+    # Pop off two empty elements in array
+    dataArr.pop(0)
+    dataArr.pop(0)
+
     file.close()
+    return dataArr
+        
+# Function to parse account information from account.js
+def account(path):
+
+    # Opens databse and initializes path names
+    # TODO - ADD FILE CHECKING
+    db = open_db(DATABASE)
+    accountPath = path + "\\account.js"
+    ipPath = path + "\\account-creation-ip.js"
+
+    # Fills arrays with data from files
+    accountData = getDict(accountPath)
+    ipData = getDict(ipPath)
+
+    fieldQuery = "CREATE TABLE Account_Info(Username, Display_Name, Email, PhoneNum, AccountID, CreatedIP, CreatedAt, Timezone, CreatedVia)"
+
+    # Executes the create table, then inserts the data
+    db.execute(fieldQuery)
+    db.execute("INSERT INTO Account_Info (Username, Display_Name, Email, PhoneNum, AccountID, CreatedIP, CreatedAt, Timezone, CreatedVia) values (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                (accountData[3], accountData[2], accountData[5], accountData[6], accountData[1],ipData[0], accountData[4], accountData[7], accountData[0]))
+
+    db.commit()
+
+def getDataFromTweet(regEx, inputStr):
+    data = re.search(regEx, inputStr)
+    if data:
+        data = data.group(0)
+        return data
+    else:
+        return "NULL"
+
+
+def tweets(path):
+    # Opens databse and initializes path names
+    # TODO - ADD FILE CHECKING
+    db = open_db(DATABASE)
+    tweetPath = path + "\\tweet.js"
+
+    retweetArr = []
+    sourceArr = []
+    favCountArr = []
+    tweetIdArr = []
+    tweetTextArr = []
+    createdAtArr = []
+    mediaUrlArr = []
+
+    # All regex expressions
+    retweetedRX = "(.*),"
+    sourceRX = "(?<=source\" : )(.*)(?=,)"
+    favoriteCountRx = "(?<=favorite_count\" : \")(.*)(?=\",)"
+    tweetIdRx = "(?<=\"id\" : \")(.*)(?=\",)"
+    tweetTextRx = "(?<=\"full_text\" : \")(.*)(?=\",)"
+    createdAtRx = "(?<=\"created_at\" : \")(.*)(?=\",)"
+    mediaUrlRx = "(?<=\"media_url_https\" : \")(.*)(?=\")"
+
+    singleTweets = open(tweetPath).read().decode("utf-8").split("\"retweeted\" : ")
+    for i, twts in enumerate(singleTweets):
+        singleTweets[i] = twts.encode("utf-8")
+    singleTweets.pop(0)
+    for i in range(0, len(singleTweets)):
+
+        # Gets Retweet bool
+        retweetArr.append(getDataFromTweet(retweetedRX, singleTweets[i].decode("utf-8")))
+
+        # Gets Source
+        sourceArr.append(getDataFromTweet(sourceRX, singleTweets[i].decode("utf-8")))
+
+        # Gets favorite count
+        # FIX SO IT RECOGNIZES AS INT
+        favCountArr.append(getDataFromTweet(favoriteCountRx, singleTweets[i].decode("utf-8")))
+
+        # Gets Tweet ID
+        tweetIdArr.append(getDataFromTweet(tweetIdRx, singleTweets[i].decode("utf-8")))
+
+        # Gets Tweet Text
+        # FIX SO IT RECOGNIZES AS INT
+        tweetTextArr.append(getDataFromTweet(tweetTextRx, singleTweets[i].decode("utf-8")))
+
+        # Gets Created At Date/Time
+        createdAtArr.append(getDataFromTweet(createdAtRx, singleTweets[i].decode("utf-8")))
+
+        # Gets media URLS in HTTPS format
+        mediaUrlArr.append(getDataFromTweet(mediaUrlRx, singleTweets[i].decode("utf-8")))
+
+        i+=1
+    
+    # Initializes & adds data to database
+    fieldQuery = "CREATE TABLE Tweets(Tweet_Text, TweetID, CreatedAt, FavCount, Source, Retweeted, MediaURL)"
+    db.execute(fieldQuery)
+    j = 0
+    for j in range(0,len(tweetTextArr)):
+        db.execute("INSERT INTO Tweets (Tweet_Text, TweetID, CreatedAt, FavCount, Source, Retweeted, MediaURL) values (?, ?, ?, ?, ?, ?, ?)", 
+                    (tweetTextArr[j], tweetIdArr[j], createdAtArr[j], favCountArr[j], sourceArr[j], retweetArr[j], mediaUrlArr[j]))
+
+    db.commit()
+    db.close()
 
 
 def main():
     archivePath = get_argument()
-
-    # Creates database for output
-    conn = sqlite3.connect(r"output.db")
     account(archivePath)
+    tweets(archivePath)
 
 
 
